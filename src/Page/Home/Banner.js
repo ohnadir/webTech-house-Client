2fa3c0cd634c28cc3bc05306215ce84bd68adcb2
ohnadir <qx.nadir@gmail.com>
2022-05-21 21:33:29 +0600

    add header on home section

---
+++ src/Page/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="">
+                <div className="flex flex-col lg:flex-row-reverse justify-between items-center">
+                    <img src="https://api.lorem.space/image/movie?w=260&h=400" className="max-w-xl rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
