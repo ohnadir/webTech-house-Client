c509ec1a1c75846e7db9b26d3950304953078433
ohnadir <qx.nadir@gmail.com>
2022-05-22 03:06:01 +0600

    implemet blogs section

--- src/Page/Blogs/Blogs.js
+++ src/Page/Blogs/Blogs.js
@@ -2,8 +2,52 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>THis is BLogs Page</h1>
+        <div className=''>
+            <div class="collapse border">
+                <input type="checkbox" class="peer" /> 
+                <div class="collapse-title ">
+                    How will you improve the performance of a React Application?
+                </div>
+                <div class="collapse-content  border border-t-1 border-b-0"> 
+                    <p className='pt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, ex. Optio explicabo dolore, excepturi tempore officia sapiente modi! Unde facere temporibus corporis assumenda dignissimos sed in repellat neque hic illo!</p>
+                </div>
+            </div>
+            <div class="collapse">
+                <input type="checkbox" class="peer" /> 
+                <div class="collapse-title border ">
+                    What are the different ways to manage a state in a React application?
+                </div>
+                <div class="collapse-content border border-x-1"> 
+                    <p className='pt-2'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis nisi necessitatibus minima repellendus? Nobis tempora libero dolor eligendi eaque, repellat impedit quae? Ipsam est dignissimos deleniti unde minus libero illo modi. Maiores, totam accusantium dolor veniam commodi neque distinctio sapiente ab eveniet voluptates, rem magnam assumenda ex, corrupti autem nobis ut error libero earum a voluptas? Natus consequatur placeat ut?</p>
+                </div>
+            </div>
+            <div class="collapse">
+                <input type="checkbox" class="peer" /> 
+                <div class="collapse-title border">
+                    How does prototypical inheritance work?
+                </div>
+                <div class="collapse-content border "> 
+                    <p className='pt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus libero culpa quia impedit asperiores harum molestias id, repellendus officia vel, facilis quod deserunt voluptates perferendis. Sit repellat laboriosam optio rerum soluta tempore vitae ratione atque eaque dicta quasi, illo magnam perspiciatis error quae ea? Deleniti est pariatur sed? Nisi dolor deleniti eveniet et, sunt nulla ab reiciendis suscipit, soluta porro eum, quaerat accusantium impedit est quas corrupti natus unde. Fuga.</p>
+                </div>
+            </div>
+            <div class="collapse">
+                <input type="checkbox" class="peer" /> 
+                <div class="collapse-title border">
+                    You have an array of products. Each object has a name, price, description, etc. How will you implement a search to find products by name?
+                </div>
+                <div class="collapse-content border "> 
+                    <p className='pt-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae perspiciatis dicta aperiam ducimus expedita deleniti harum provident at? Illum inventore nam dignissimos quas ullam mollitia, eum cum tempore recusandae. Eaque placeat doloremque nesciunt perferendis quos autem obcaecati magnam voluptatibus, mollitia aut aliquid a nam adipisci odio magni blanditiis consectetur ut itaque accusantium totam reiciendis. Sunt unde dolorum optio, expedita, sint voluptates velit dolor ea porro ullam doloremque possimus quibusdam amet perspiciatis soluta excepturi cum iusto doloribus? Minima natus beatae delectus.</p>
+                </div>
+            </div>
+            <div class="collapse">
+                <input type="checkbox" class="peer" /> 
+                <div class="collapse-title border">
+                    What is a unit test? Why should write unit tests?
+                </div>
+                <div class="collapse-content border "> 
+                    <p className='pt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint odio impedit, soluta quibusdam saepe tempora ullam voluptatum. Maxime quos quidem, illo natus officiis nobis dolore nisi, iure et non at sequi reiciendis enim pariatur soluta, aliquid voluptatem rem laborum aperiam earum cupiditate. Animi accusantium harum illum iure in ipsam architecto vitae inventore ut officia sit, voluptatum facilis voluptate cupiditate aperiam! Optio similique doloremque expedita repellat sed iure adipisci. Necessitatibus numquam possimus nihil iste accusamus dolores quasi quis minus placeat vero itaque praesentium illum a illo tenetur, dolor nisi dicta magni, aliquam earum nulla commodi, blanditiis velit vitae? Magnam, recusandae labore!</p>
+                </div>
+            </div>
         </div>
     );
 };
